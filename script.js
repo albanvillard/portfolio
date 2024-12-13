3a1d02c9885078816d8b7cfe254a8903b2d0d326
Villard <alban.villad@saint-benigne.fr>
2024-12-13 08:31:22 +0100

    pushecole3

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-// Action pour le bouton d'alerte
-document.getElementById("alertButton").addEventListener("click", function () {
-    alert("Vous avez cliqué sur le bouton !");
-});
-
-// Action pour changer la couleur de fond
-document.getElementById("colorButton").addEventListener("click", function () {
-    // Génère une couleur aléatoire
-    const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-    document.body.style.backgroundColor = randomColor;
-});
-
-
 //step 1: get DOM
 let nextDom = document.getElementById('next');
 let prevDom = document.getElementById('prev');
@@ -37,6 +24,7 @@ let runNextAuto = setTimeout(() => {
     next.click();
 }, timeAutoNext)
 function showSlider(type){
+    console.log("TYPE:"+type);
     let  SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
     let thumbnailItemsDom = document.querySelectorAll('.carousel .thumbnail .item');
     
